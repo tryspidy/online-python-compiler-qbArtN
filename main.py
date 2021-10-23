bc39484ad3e7930905b577f9a2119589d5d6e9d2
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-23 08:29:46 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+Use [repl.it](https://repl.it/)!
+Try out the basics of Replit with our 
+interactive playground. 
+Code, collaborate, compile, run, share, and deploy 
+Python and more online from your browser 
